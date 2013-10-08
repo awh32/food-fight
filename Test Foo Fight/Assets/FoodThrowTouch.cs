36f79042426c03e0fbaf0a585d38a4997723ed00
awh32 <awh32@case.edu>
2013-10-08 16:56:33 -0400

    Added a touch throw script, cnot tested, bc no ipad. Same witha player movement script

---
+++ Test Foo Fight/Assets/FoodThrowTouch.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodThrowTouch : MonoBehaviour {
+	
+	public static GameObject Player;
+	//public bool grabbed = false; 
+	public float speed = 0.1f;
+	Vector3 lastPosition = Player.transform.position;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+		//throw food on flick
+         if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			rigidbody.AddForce(touchDeltaPosition.x * speed, touchDeltaPosition.y * speed, 0);
+		}
+		
+		//move the food object with the player so the don't look disconnected
+		if(Player.transform.position.x > lastPosition.x){
+			Vector3 temp = this.transform.position;
+			temp.x = temp.x + 1;
+			this.transform.position = temp;
+		}
+		
+		if(Player.transform.position.x < lastPosition.x){
+			Vector3 temp = this.transform.position;
+			temp.x = temp.x - 1;
+			this.transform.position = temp;
+		}
+		//update player position	
+		lastPosition = Player.transform.position;
+		
+    }
+}
--- Test Foo Fight/Assets/PlayerScript.cs
+++ Test Foo Fight/Assets/PlayerScript.cs
@@ -2,14 +2,28 @@
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
-
+	
+	public GameObject FirendlyFood;
+	Vector3 tilt = Input.acceleration;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 temp;
+		
+		if(tilt.x > 0){
+			temp = this.transform.position;
+			temp.x = temp.x+1;
+			this.transform.position = temp;
+		}
+		if(tilt.x < 0){
+			temp = this.transform.position;
+			temp.x = temp.x-1;
+			this.transform.position = temp;
+		}
 		
 		//if swiping = true
 		//check if swipe trail hit enemy projectile
