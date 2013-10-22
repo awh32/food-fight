1a3f41364b7b4b524f1cd6aa2952c0c54a9c256d
awh32 <awh32@case.edu>
2013-10-22 12:16:00 -0400

    First attempt at pause and reset

---
+++ Test Foo Fight/Assets/HUDPause.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDPause : MonoBehaviour {
+	
+	public GameObject pauseMenu;
+	// Use this for initialization
+	void Start () {
+		//Load art mesh
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space)){
+			pauseMenu.SetActive(true);
+
+		}
+	}
+}
---
+++ Test Foo Fight/Assets/PopUpPause.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopUpPause : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.SetActive("false");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Application.loadLevel(0);
+		}
+	}
+}
