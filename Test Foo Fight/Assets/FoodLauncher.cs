89d8550f1972519586cbcf13c01bb8c986fc5389
awh32 <awh32@case.edu>
2013-10-29 15:12:48 -0400

    Made launching script: untested

---
+++ Test Foo Fight/Assets/FoodLauncher.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodLauncher : MonoBehaviour {
+	
+	//assign current food object to be launched
+	public GameObject food;
+	public float launchspeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		// Casts the ray and get the first game object hit
+		Physics.Raycast(ray, out hit);
+		if(hit.rigidbody == this.rigidbody){
+			if(Input.GetMouseButtonDown(0)){
+				food.rigidbody.AddForce(0,launchspeed,0);
+			}
+		}
+	}
+}
--- Test Foo Fight/Assets/PopUpPause.cs
+++ Test Foo Fight/Assets/PopUpPause.cs
@@ -2,29 +2,35 @@
 using System.Collections;
 
 public class PopUpPause : MonoBehaviour {
-
+	
+	bool rendered = false;
 	// Use this for initialization
 	void Start () {
+		//keeps the menu invisible at first
 		this.gameObject.renderer.enabled = false;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0)){
+		//reset level
+		if(Input.GetMouseButtonDown(0) && rendered){
 			Application.LoadLevel(0);
 		}
-		if(Input.GetMouseButtonDown(1)){
+		//unpause
+		if(Input.GetMouseButtonDown(1) && rendered){
 			Time.timeScale = 1;
 			this.gameObject.renderer.enabled = false;
 			
 		}
-		if(Input.GetKey(KeyCode.A)){
+		//sound control
+		if(Input.GetKey(KeyCode.A) && rendered){
 			AudioListener.volume = AudioListener.volume - 0.1f;
 			if(AudioListener.volume < 0f){
 				AudioListener.volume = 0f;
 			}
 		}
-		if(Input.GetKey(KeyCode.S)){
+		if(Input.GetKey(KeyCode.S) && rendered){
 			AudioListener.volume = AudioListener.volume + 0.1f;
 			if(AudioListener.volume > 1f){
 				AudioListener.volume = 1f;
